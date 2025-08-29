6d1c284693e5048afa87d5dde8e3d0c4a81e4fdf
buraksefil <buraksefil@yahoo.com>
2025-08-29 13:18:32 +0200

    first upload

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "eksterrabucket"
+    bucket = "argocd-demo-bucket123"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
-    dynamodb_table = "dynamoDB-terra"
+    dynamodb_table = "dynamodb-argocd-demo123"
   }
 }
